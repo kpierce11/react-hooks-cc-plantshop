215347200cf8438ba39a84cdcb045d6a2768b7a0
kpierce11 <kpierce1@samford.edu>
2025-04-18 02:33:03 -0400

    makes search bar controlled and renders matching plants

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,14 +1,15 @@
 import React from "react";
 
-function Search() {
+function Search({ searchQuery, onSearchChange }) {
   return (
     <div className="searchbar">
       <label htmlFor="search">Search Plants:</label>
       <input
         type="text"
         id="search"
+        value={searchQuery}
+        onChange={(e) => onSearchChange(e.target.value)}
         placeholder="Type a name to search..."
-        onChange={(e) => console.log("Searching...")}
       />
     </div>
   );
