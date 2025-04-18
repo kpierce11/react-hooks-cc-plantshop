4a43243db48f4729baa822aec734cf71fd095589
kpierce11 <kpierce1@samford.edu>
2025-04-18 01:40:18 -0400

    creates state and fetches data in PlantPage

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -2,8 +2,18 @@ import React from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
+import { useState, useEffect } from "react";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([]);
+  const [searchQuery , setSearchQuery] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((r) => r.json())
+      .then((data) => setPlants(data.map((plant) => ({...plant, isSoldOut: false}))));
+  }, []);
+  
   return (
     <main>
       <NewPlantForm />
